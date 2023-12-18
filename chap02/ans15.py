852bc2953b5658988247a7259b8e3e5f1a945349
hyphen <s21t343@kagawa-u.ac.jp>
2023-12-18 11:52:39 +0900

    15番完成

--- chap02/ans15.py
+++ chap02/ans15.py
@@ -1,14 +1,12 @@
 import sys
 
-file_path = "popular-names.txt"
-
 if len(sys.argv) != 2:
-    print("Usage: python script.py <N>")
+    print("引数不足")
     sys.exit(1)
 
 n = int(sys.argv[1])
 
-with open(file_path, "r", encoding="utf-8") as f:
+with open("popular-names.txt", "r", encoding="utf-8") as f:
     lines = f.readlines()
     for line in lines[-n:]:
         print(line.strip())
