352e58c3a3a83985eef7f2070a25ba365b04ed18
hyphen <s21t343@kagawa-u.ac.jp>
2023-12-18 11:17:03 +0900

    10番完成

--- chap02/ans10.py
+++ chap02/ans10.py
@@ -1,17 +1,9 @@
-import sys
-
-# コマンドライン引数でファイル名を指定
-file_name = sys.argv[1]
-
 # ファイルを読み込む
-with open(file_name, "r") as f:
+with open("popular-names.txt", "r") as f:
     text = f.read()
 
-# 行数と文字数をカウント
+# 行数カウント
 lines = text.split("\n") #テキストを行ごとに分割
-line_count = len(lines)
+count = len(lines)
 
-print(f"行数: {line_count}")
-
-#確認方法wc -l popular-names.txt
-#1少なく表示されたためtxt最終行に改行追加
+print(f"行数: {count}")
---
+++ chap02/ans10.sh
@@ -0,0 +1,3 @@
+wc -l popular-names.txt
+#なぜか1小さくなる
+#popular-names.txtの最終行を改行させたら合う。
