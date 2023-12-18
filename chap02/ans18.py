4398d585142c3a1464b881a3cf8ec68957da9865
hyphen <s21t343@kagawa-u.ac.jp>
2023-12-18 12:19:21 +0900

    18番完成

--- chap02/ans18.py
+++ chap02/ans18.py
@@ -1,10 +1,10 @@
-file_path = 'popular-names.txt'
 
-with open(file_path, 'r', encoding='utf-8') as file:
+with open('popular-names.txt', 'r', encoding='utf-8') as file:
     lines = file.readlines()
 
 # 3列目の数値でソート
 sorted_lines = sorted(lines, key=lambda line: int(line.split()[2]), reverse=True)
+#True = 降順ソート
 
 # ソートされた結果をファイルに保存
 with open("ans18.txt", 'w', encoding='utf-8') as output_file:
