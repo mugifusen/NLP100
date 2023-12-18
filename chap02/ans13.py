63ec104c6e0ed1bf18f17c05c6982d06fa8abae3
hyphen <s21t343@kagawa-u.ac.jp>
2023-12-18 11:41:53 +0900

    13番完成

--- chap02/ans13.py
+++ chap02/ans13.py
@@ -8,3 +8,4 @@ with open('col1.txt', 'r', encoding='utf-8') as f1, \
     for line_output1, line_output2 in zip(lines_output1, lines_output2):#zipでまとめて1つのイテレータ作成
         ansf.write(line_output1.strip() + '\t' + line_output2.strip() + '\n')
         #stripメソッドで各行の先頭と末尾の空白文字を削除
+        
