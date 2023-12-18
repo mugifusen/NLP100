ceb5989608a28776d115d771f7b59425a5d4ca57
hyphen <s21t343@kagawa-u.ac.jp>
2023-12-18 11:25:07 +0900

    11番完成

--- chap02/ans11.py
+++ chap02/ans11.py
@@ -1,10 +1,12 @@
-# ファイルを読み込む
+# ファイルを読み込みモードで読み込む
 with open("popular-names.txt", "r") as f:
     text = f.read()
+    #readメソッド = ファイルの中身を文字列として変換
 
 output = text.replace("\t"," ")
+#replace = 置換（textのタブ文字を空白文字に）
 
-with open("output.txt",'w') as writer:
-    writer.write(output)
+with open("output.txt",'w') as w:
+    w.write(output)#新しい文字列outputをoutput.txtに書きこむ。
 
-#sed -e 's/\t/ /g' ./popular-names.txt
+
---
+++ chap02/ans11.sh
@@ -0,0 +1,10 @@
+sed -e "s/\t/ /g" ./popular-names.txt
+#https://blog.brains-consulting.tech/entry/2022/08/31/173834#%E8%A7%A3%E7%AD%94%E3%82%B3%E3%83%BC%E3%83%89Python-1
+#ここからとってきましたわかんない
+
+#sed = テキスト変更を行うためのコマンド
+#-e = スクリプト指定コマンド
+# s = 置換
+# '\t' = タブ文字
+# / = 区切り
+# /g = 行全体に対する全置換
