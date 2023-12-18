e84d20ae11d4ce9a4abcec6bfda090a813845118
hyphen <s21t343@kagawa-u.ac.jp>
2023-12-18 11:49:28 +0900

    14番完成

--- chap02/ans14.py
+++ chap02/ans14.py
@@ -1,14 +1,12 @@
-import sys
-
-file_path = "popular-names.txt"
+import sys#コマンドライン引数の取得
 
 if len(sys.argv) != 2:
-    print("Usage: python script.py <N>")
+    print("引数不足")
     sys.exit(1)
 
 n = int(sys.argv[1])
 
-with open(file_path, "r", encoding="utf-8") as f:
-    for i, line in enumerate(f):
+with open("popular-names.txt", "r", encoding="utf-8") as f:
+    for i, line in enumerate(f):#enumerate = 要素のインデックスと要素を同時に取り出す
         if i < n:
             print(line.strip())
---
+++ chap02/ans14.sh
@@ -0,0 +1,2 @@
+head -n 10 popular-names.txt
+#-n 表示する行数を指定→なかったらエラー出るけどでてきた
